f83917b8b74f494c5ba7619ac16c9bd64d8a9be7
HelinaBerhane <fae1029@hotmail.co.uk>
2016-10-15 12:41:55 +0100

    Ask for input

--- Timer.py
+++ Timer.py
@@ -12,21 +12,30 @@ connection = sqlite3.connect('budget.db')
 # create a Cursor object and call its execute() method to perform SQL commands:
 c = connection.cursor()
 # Create the tables
-c.execute("CREATE TABLE IF NOT EXISTS time(type STRING(50), task STRING(100), dailyAmount FLOAT, notes STRING(1000))")
+c.execute("CREATE TABLE IF NOT EXISTS time(type STRING(50), task STRING(100), time FLOAT, notes STRING(1000))")
 
 #---------------------#
 #----- Functions -----#
 #---------------------#
 
-def mean():
+#def mean():
     # calculates the mean
     # = sum()/count()
 
 
-def stanDev():
+#def stanDev():
     # calculates the standard deviation
     # sd = sqrt (sum(|x-x\bar|^2)/n)
 
+while True:
+    Question = input("Do you want to log a time? y/n: ")
+    if Question == "y":
+        typeQuestion = input("What type of task was it? e.g. Travel/Prep/Housework - ")
+        taskQuestion = input("What task do you want to log? e.g. Travelling to uni -  ")
+        timeQuestion = input("How long did it take you? [hh]:mm:ss - ")
+        print("Type: " + typeQuestion + ". Task: " + taskQuestion + ". Question: " + timeQuestion)
+    else:
+        break
 
 #---------#
 #-- End --#
