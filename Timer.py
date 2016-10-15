ed396b09377ddf78547e4fb26a9c3dceba9efdcb
HelinaBerhane <fae1029@hotmail.co.uk>
2016-10-15 12:54:06 +0100

    Logged to database

--- Timer.py
+++ Timer.py
@@ -8,11 +8,11 @@ import sqlite3 # databases
 #--------------------#
 
 # create the connection object that represents the database
-connection = sqlite3.connect('budget.db')
+connection = sqlite3.connect('time.db')
 # create a Cursor object and call its execute() method to perform SQL commands:
 c = connection.cursor()
 # Create the tables
-c.execute("CREATE TABLE IF NOT EXISTS time(type STRING(50), task STRING(100), time FLOAT, notes STRING(1000))")
+c.execute("CREATE TABLE IF NOT EXISTS time(type STRING(50), task STRING(100), time FLOAT)")
 
 #---------------------#
 #----- Functions -----#
@@ -28,13 +28,33 @@ c.execute("CREATE TABLE IF NOT EXISTS time(type STRING(50), task STRING(100), ti
     # sd = sqrt (sum(|x-x\bar|^2)/n)
 
 while True:
-    Question = input("Do you want to log a time? y/n: ")
-    if Question == "y":
+    
+    # ask for intention
+    question = input("Do you want to log a time? y/n - ")
+    if question == "y":
+        
+        # ask for inputs
         typeQuestion = input("What type of task was it? e.g. Travel/Prep/Housework - ")
         taskQuestion = input("What task do you want to log? e.g. Travelling to uni -  ")
         timeQuestion = input("How long did it take you? [hh]:mm:ss - ")
-        print("Type: " + typeQuestion + ". Task: " + taskQuestion + ". Question: " + timeQuestion)
+        print("Type: " + typeQuestion + "\nTask: " + taskQuestion + "\nQuestion: " + timeQuestion)
+
+        # add the inputs to the database
+        logArr = [typeQuestion, taskQuestion, timeQuestion]
+        c.execute("INSERT INTO time VALUES (?,?,?)", logArr)
+
+        # commit changes
+        connection.commit()
+        
     else:
+        # ask for intention
+        question2 = input("do you want to view your current logs? y/n - ")
+        if question2 == "y":
+            
+            # Print sources of income
+            print("Your sources of spending are:")
+            for row in c.execute("SELECT * FROM time"):
+	            print(row)
         break
 
 #---------#
